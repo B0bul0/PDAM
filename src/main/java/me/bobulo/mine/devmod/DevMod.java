a21bed2dc3e70b70c774d3640e3e5bfa79713cec
Bruno Rezende <email.bruno0@gmail.com>
2025-09-07 21:47:03 -0300

    add logger for initialization message

--- src/main/java/me/bobulo/mine/devmod/DevMod.java
+++ src/main/java/me/bobulo/mine/devmod/DevMod.java
@@ -4,13 +4,17 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "devmod", useMetadata = true)
 public class DevMod {
 
+    private static final Logger log = LogManager.getLogger(DevMod.class);
+
     @EventHandler
     public void init(FMLPreInitializationEvent event) {
-        System.out.println("DevMod initialized");
+        log.info("DevMod initialized");
     }
 
 }
