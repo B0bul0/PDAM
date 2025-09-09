7b558e809e10b79d461a4a3f5cb57ed859b8a1a7
Bruno Rezende <email.bruno0@gmail.com>
2025-09-09 13:11:54 -0300

    add ForgerListenerFeatureComponent to register NBTTagTooltipListener feature

--- src/main/java/me/bobulo/mine/devmod/DevMod.java
+++ src/main/java/me/bobulo/mine/devmod/DevMod.java
@@ -4,6 +4,7 @@ import me.bobulo.mine.devmod.config.ConfigListener;
 import me.bobulo.mine.devmod.config.ConfigurationService;
 import me.bobulo.mine.devmod.feature.FeatureImpl;
 import me.bobulo.mine.devmod.feature.FeatureService;
+import me.bobulo.mine.devmod.feature.component.ForgerListenerFeatureComponent;
 import me.bobulo.mine.devmod.feature.sound.SoundDebugFeatureComponent;
 import me.bobulo.mine.devmod.feature.tooltop.NBTTagTooltipListener;
 import net.minecraftforge.common.MinecraftForge;
@@ -59,6 +60,11 @@ public class DevMod {
         featureService.registerFeature(FeatureImpl.builder()
           .id("sound_debug")
           .component(new SoundDebugFeatureComponent())
+          .build());
+
+        featureService.registerFeature(FeatureImpl.builder()
+          .id("nbt_tooltip")
+          .component(ForgerListenerFeatureComponent.of(new NBTTagTooltipListener()))
           .build());
     }
 
