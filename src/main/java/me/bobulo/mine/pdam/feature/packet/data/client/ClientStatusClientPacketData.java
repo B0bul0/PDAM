97b4deba62223b4b64822aa9715114380fc7f590
Bruno Rezende <email.bruno0@gmail.com>
2026-03-17 20:25:12 -0300

    Refactor ClientStatusClientPacketData to use State enum for status handling

--- src/main/java/me/bobulo/mine/pdam/feature/packet/data/client/ClientStatusClientPacketData.java
+++ src/main/java/me/bobulo/mine/pdam/feature/packet/data/client/ClientStatusClientPacketData.java
@@ -4,19 +4,40 @@ import me.bobulo.mine.pdam.feature.packet.data.reader.PacketDataExtractor;
 import net.minecraft.network.play.client.C16PacketClientStatus;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.EnumMap;
+
 public final class ClientStatusClientPacketData implements ClientPacketData {
 
-    private C16PacketClientStatus.EnumState status;
+    private static final EnumMap<C16PacketClientStatus.EnumState, State> MAP =
+      new EnumMap<>(C16PacketClientStatus.EnumState.class);
+
+    static {
+        for (C16PacketClientStatus.EnumState s : C16PacketClientStatus.EnumState.values()) {
+            try {
+                MAP.put(s, State.valueOf(s.name()));
+            } catch (IllegalArgumentException ignored) {
+                // ignored, if the enum value is not present in our State enum
+            }
+        }
+    }
+
+    private State status;
 
     public static class Extractor implements PacketDataExtractor<ClientStatusClientPacketData, C16PacketClientStatus> {
 
         @Override
         public @NotNull ClientStatusClientPacketData extract(@NotNull C16PacketClientStatus packet) {
             ClientStatusClientPacketData data = new ClientStatusClientPacketData();
-            data.status = packet.getStatus();
+            data.status = MAP.get(packet.getStatus());
             return data;
         }
 
+    }
+
+    public enum State {
+        PERFORM_RESPAWN,
+        REQUEST_STATS,
+        OPEN_INVENTORY_ACHIEVEMENT;
     }
 
 }
