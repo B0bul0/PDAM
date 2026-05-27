59aefad1df2cb44e70e21f43fd89ff3fc80ce9b2
Bruno Rezende <email.bruno0@gmail.com>
2026-05-27 13:04:16 -0300

    Add documentation for ToolbarItemWindow

--- src/main/java/me/bobulo/mine/pdam/imgui/toolbar/ToolbarItemWindow.java
+++ src/main/java/me/bobulo/mine/pdam/imgui/toolbar/ToolbarItemWindow.java
@@ -1,5 +1,8 @@
 package me.bobulo.mine.pdam.imgui.toolbar;
 
+/**
+ * Represents a window that can be toggled from the toolbar menu in ImGui.
+ */
 public interface ToolbarItemWindow {
 
     String getMenuName();
